d4866b3cf19b37bddd77324da7492fe0a2d241ef
inti0292 <inti0292@DESKTOP-QTEOOPT.home>
2016-11-24 14:30:01 +0100

    Thomas : Commit Compte

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/service/CompteServiceImpl.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/service/CompteServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import fr.adaming.dao.ICompteDao;
 import fr.adaming.model.Compte;
 
-@Service
+@Service("compteService")
 @Transactional
 public class CompteServiceImpl implements ICompteService {
 
