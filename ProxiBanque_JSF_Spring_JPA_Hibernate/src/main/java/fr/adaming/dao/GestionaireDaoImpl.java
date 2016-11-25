583ff12e2454a1b2d5034d5f3a244d8527364069
inti0293 <inti0293@DESKTOP-4C50ANM.home>
2016-11-25 14:33:34 +0100

    Fabien : fonction gestionnaire faite t'es content ??

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/dao/GestionaireDaoImpl.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/dao/GestionaireDaoImpl.java
@@ -14,6 +14,7 @@ import javax.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import fr.adaming.model.Client;
 import fr.adaming.model.Gestionnaire;
 
 /**
@@ -24,48 +25,60 @@ import fr.adaming.model.Gestionnaire;
 @Repository
 public class GestionaireDaoImpl implements IGestionnaireDao {
 	
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------1_Les propriétés (champs, attributs)-------------------------------------------
-		/**
-		 * 1_Les propriétés (champs, attributs)
-		 */
-	
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------1_Les propriétés (champs, attributs)-------------------------------------------
+	/**
+	 * 1_Les propriétés (champs, attributs)
+	 */
+
 	@PersistenceContext(name="ProxyBanque")
 	EntityManager em;
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------2_Les constructeurs------------------------------------------------------------	
-		/**
-		 * 2_Les constructeurs
-		 */
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------3_Les Getters et Setters-------------------------------------------------------
-		/**
-		 * 3_Les Getters et Setters
-		 */
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------4_Méthodes---------------------------------------------------------------------
-		/**
-		 * 4_Méthodes
-		 */
-	//----------------------------------------------------------------------------------------------------------------
-	//----------------------------------------------------------------------------------------------------------------
-
-	
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------2_Les constructeurs------------------------------------------------------------	
+	/**
+	 * 2_Les constructeurs
+	 */
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------3_Les Getters et Setters-------------------------------------------------------
+	/**
+	 * 3_Les Getters et Setters
+	 */
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------4_Méthodes---------------------------------------------------------------------
+	/**
+	 * 4_Méthodes
+	 */
 	@Override
 	public int isExistGestionnaireDao(String login, String password) {
+	
+	System.out.println("Toto");
+	
+	String req="SELECT * FROM gestionnaire as g WHERE g.login =:login and g.password=:password";
+	
+	Query queryGestionnaireIsExist = em.createNativeQuery(req);
+	queryGestionnaireIsExist.setParameter("login",login);
+	queryGestionnaireIsExist.setParameter("password",password);
+	
+	@SuppressWarnings("unchecked")
+	List<Gestionnaire> listeGestionnaire = queryGestionnaireIsExist.getResultList();
+			System.out.println(listeGestionnaire.size());
+	return listeGestionnaire.size();
+}
+	@Override
+	public Gestionnaire getGestByIdentificationDao(String login, String password) {
+		String req="SELECT g FROM Gestionnaire g WHERE g.login=:login and g.password=:password";
 		
-		System.out.println("Toto");
+		Query query =em.createQuery(req);
+		query.setParameter("login", login);
+		query.setParameter("password", password);
 		
-		String req="SELECT * FROM gestionnaire as g WHERE g.login =:login and g.password=:password";
-		
-		Query queryGestionnaireIsExist = em.createNativeQuery(req);
-		queryGestionnaireIsExist.setParameter("login",login);
-		queryGestionnaireIsExist.setParameter("password",password);
-		
-		@SuppressWarnings("unchecked")
-		List<Gestionnaire> listeGestionnaire = queryGestionnaireIsExist.getResultList();
-				System.out.println(listeGestionnaire.size());
-		return listeGestionnaire.size();
+		Gestionnaire gestionnaire =  (Gestionnaire) query.getSingleResult();
+		return gestionnaire;
 	}
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+
+	
+
 
 }
