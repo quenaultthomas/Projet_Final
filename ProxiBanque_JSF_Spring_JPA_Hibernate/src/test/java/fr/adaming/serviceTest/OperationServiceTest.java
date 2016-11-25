a8f75185c0175ad4a67ca062a463588d1c107bbf
Tchement <tchement.enseirb@gmail.com>
2016-11-25 12:27:31 +0100

    OperationTestService all commented

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/test/java/fr/adaming/serviceTest/OperationServiceTest.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/test/java/fr/adaming/serviceTest/OperationServiceTest.java
@@ -1,36 +1,36 @@
-package fr.adaming.serviceTest;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.Calendar;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import fr.adaming.model.Compte;
-import fr.adaming.model.Operation;
-import fr.adaming.service.IOperationService;
-
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/application-context.xml")
-//public class OperationServiceTest {
+//package fr.adaming.serviceTest;
 //
-//	@Autowired
-//	IOperationService operationService;
-//	
-//	@Test
-//	public void testAjouterOperation(){
-//		Calendar c = Calendar.getInstance();
-//		
-//		System.out.println("je suis dans la methode testAjouterOperation");
-//		Operation operation = new Operation("Virement", 1200, c.getTime());
-//		Compte compte = new Compte();
-//		compte.setId_compte(1);
-//		operation.setCompte(compte);
-//		operationService.ajouterOperationService(operation);
-//		assertEquals(1, operationService.getOperationsByIdCompteService(1).size());
-//	}
-//}
+//import static org.junit.Assert.assertEquals;
+//
+//import java.util.Calendar;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import fr.adaming.model.Compte;
+//import fr.adaming.model.Operation;
+//import fr.adaming.service.IOperationService;
+//
+////@RunWith(SpringJUnit4ClassRunner.class)
+////@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/application-context.xml")
+////public class OperationServiceTest {
+////
+////	@Autowired
+////	IOperationService operationService;
+////	
+////	@Test
+////	public void testAjouterOperation(){
+////		Calendar c = Calendar.getInstance();
+////		
+////		System.out.println("je suis dans la methode testAjouterOperation");
+////		Operation operation = new Operation("Virement", 1200, c.getTime());
+////		Compte compte = new Compte();
+////		compte.setId_compte(1);
+////		operation.setCompte(compte);
+////		operationService.ajouterOperationService(operation);
+////		assertEquals(1, operationService.getOperationsByIdCompteService(1).size());
+////	}
+////}
