60df5014a14c2006c214d0cc1a1df31773d2cdde
inti0304 <inti0304@DESKTOP-RT65F4M.home>
2016-11-25 10:32:23 +0100

    Max : service gest en +

---
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/test/java/fr/adaming/daoTest/GestionnaireDaoTest.java
@@ -0,0 +1,39 @@
+//package fr.adaming.daoTest;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import fr.adaming.dao.IGestionnaireDao;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/application-context.xml")
+//public class GestionnaireDaoTest {
+//	
+//	@Autowired
+//	IGestionnaireDao gestionDao;
+//	
+//	@Before
+//	public void beforeClassTest() {
+//
+//		System.out.println("\n Je suis dans la méthode Before Class");
+//	}
+//
+//	@After
+//	public void afterTest() {
+//
+//		System.out.println("\n Je suis dans la méthode After");
+//	}
+	
+//	@Test
+//	public void testIsExist(){
+//		
+//		assertEquals(1, gestionDao.isExistGestionnaireDao("a", "a"));
+//		System.out.println("Je suis dans la méthode isExist");
+//	}
+//
+//}
