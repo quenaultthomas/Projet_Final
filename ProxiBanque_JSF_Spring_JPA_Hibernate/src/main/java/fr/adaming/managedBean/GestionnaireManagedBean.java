6cc97a4fa720e24b7a9452b1e428b27b416c61de
Tchement <tchement.enseirb@gmail.com>
2016-11-27 16:17:22 +0100

    Add Client issue fixed ! Thanks Victor.

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/managedBean/GestionnaireManagedBean.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/managedBean/GestionnaireManagedBean.java
@@ -21,18 +21,19 @@ import fr.adaming.service.ICompteService;
 import fr.adaming.service.IGestionnaireService;
 import fr.adaming.service.IOperationService;
 
-@ManagedBean(name="GestMB")
+@ManagedBean(name = "GestMB")
 @SessionScoped
-public class GestionnaireManagedBean implements Serializable{
+public class GestionnaireManagedBean implements Serializable {
 
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------1_Les propriétés (champs, attributs)-------------------------------------------
-		/**
-		 * 1_Les propriétés (champs, attributs)
-		 */
-	
+	// ----------------------------------------------------------------------------------------------------------------
+	// ---------------------------------1_Les propriétés (champs,
+	// attributs)-------------------------------------------
+	/**
+	 * 1_Les propriétés (champs, attributs)
+	 */
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private int id_gestionnaire;
 	private Compte cpt;
 	private Compte cpt2;
@@ -43,109 +44,87 @@ public class GestionnaireManagedBean implements Serializable{
 	private Carte carte;
 	private Gestionnaire gestionnaire;
 
-	
 	private List<Client> listClient;
 	private List<Gestionnaire> listeGestionnaires;
 	private List<Compte> listCpt;
 
 	@ManagedProperty(value = "#{compteService}")
 	ICompteService compteService;
-	
+
 	@ManagedProperty(value = "#{clientService}")
 	IClientService clientService;
-	
+
 	@ManagedProperty(value = "#{carteService}")
 	ICarteService carteService;
-	
+
 	@ManagedProperty(value = "#{operationService}")
 	IOperationService operationService;
-	
+
 	@ManagedProperty(value = "#{gestionService}")
 	IGestionnaireService gestionnaireService;
-	
-	
+
 	HttpSession session;
 
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------2_Les constructeurs------------------------------------------------------------	
-		/**
-		 * 2_Les constructeurs
-		 */
-	
+	// ----------------------------------------------------------------------------------------------------------------
+	// ---------------------------------2_Les
+	// constructeurs------------------------------------------------------------
+	/**
+	 * 2_Les constructeurs
+	 */
+
 	public GestionnaireManagedBean() {
 		this.cpt = new Compte();
-		this.client= new Client();
-		this.carte= new Carte();
+		this.client = new Client();
+		this.carte = new Carte();
 		this.gestionnaire = new Gestionnaire();
 	}
 
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------3_Les Getters et Setters-------------------------------------------------------
-		/**
-		 * 3_Les Getters et Setters
-		 */
+	// ----------------------------------------------------------------------------------------------------------------
+	// ---------------------------------3_Les Getters et
+	// Setters-------------------------------------------------------
+	/**
+	 * 3_Les Getters et Setters
+	 */
 
-	
-	
 	public Compte getCpt() {
 		return cpt;
 	}
-
-
 
 	public void setCpt(Compte cpt) {
 		this.cpt = cpt;
 	}
 
-
-
 	public Compte getCpt2() {
 		return cpt2;
 	}
-
-
 
 	public void setCpt2(Compte cpt2) {
 		this.cpt2 = cpt2;
 	}
 
-
-
 	public int getId() {
 		return id;
 	}
-
-
 
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
-
 	public int getId2() {
 		return id2;
 	}
-
-
 
 	public void setId2(int id2) {
 		this.id2 = id2;
 	}
 
-
-
 	public double getMontant() {
 		return montant;
 	}
 
-
-
 	public void setMontant(double montant) {
 		this.montant = montant;
 	}
-
-
 
 	public Gestionnaire getGestionnaire() {
 		return gestionnaire;
@@ -159,37 +138,25 @@ public class GestionnaireManagedBean implements Serializable{
 		return client;
 	}
 
-
-
 	public void setClient(Client client) {
 		this.client = client;
 	}
-
-
 
 	public Carte getCarte() {
 		return carte;
 	}
 
-
-
 	public void setCarte(Carte carte) {
 		this.carte = carte;
 	}
-
-
 
 	public List<Client> getListClient() {
 		return listClient;
 	}
 
-
-
 	public void setListClient(List<Client> listClient) {
 		this.listClient = listClient;
 	}
-
-
 
 	public List<Compte> getListCpt() {
 		return listCpt;
@@ -203,7 +170,8 @@ public class GestionnaireManagedBean implements Serializable{
 	}
 
 	/**
-	 * @param listeGestionnaires the listeGestionnaires to set
+	 * @param listeGestionnaires
+	 *            the listeGestionnaires to set
 	 */
 	public void setListeGestionnaires(List<Gestionnaire> listeGestionnaires) {
 		this.listeGestionnaires = listeGestionnaires;
@@ -233,18 +201,15 @@ public class GestionnaireManagedBean implements Serializable{
 		this.listCpt = listCpt;
 	}
 
-
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-	//----------------------------------------------------------------------------------------------------------------
-	//---------------------------------4_Méthodes---------------------------------------------------------------------
-		/**
-		 * 4_Méthodes
-		 */
-
+	// ----------------------------------------------------------------------------------------------------------------
+	// ---------------------------------4_Méthodes---------------------------------------------------------------------
+	/**
+	 * 4_Méthodes
+	 */
 
 	@Override
 	public String toString() {
@@ -252,51 +217,48 @@ public class GestionnaireManagedBean implements Serializable{
 				+ montant + ", client=" + client + ", carte=" + carte + ", listClient=" + listClient + ", listCpt="
 				+ listCpt + "]";
 	}
-	
+
 	@PostConstruct
 	private void init() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		session = (HttpSession) facesContext.getExternalContext().getSession(false);
-		
-		
-//		listClient = clientService.getClientsByIdGestionnaireService(gestionnaire.getId_gestionnaire());
+
+		// listClient =
+		// clientService.getClientsByIdGestionnaireService(gestionnaire.getId_gestionnaire());
 	}
-	
-	
-	public void rechercherDebiteur(){
+
+	public void rechercherDebiteur() {
 		cpt = compteService.getCompteById(cpt.getId_compte());
 	}
-	
-	public void rechercherCrediteur(){
+
+	public void rechercherCrediteur() {
 		cpt2 = compteService.getCompteById(cpt2.getId_compte());
 	}
 
-	
-	
-	
-	public String virement(){
+	public String virement() {
 		compteService.virement(montant, id, id2);
 		listCpt = compteService.getCompteByIdCLient(client.getId_client());
 		return "listeCompte.xhtml";
 	}
-	
-	public String depot(){
+
+	public String depot() {
 		compteService.depot(montant, cpt.getId_compte());
 		return "accueil.xhtml";
 	}
-	
-	public String retrait(){
+
+	public String retrait() {
 		compteService.retrait(montant, cpt.getId_compte());
 		return null;
 	}
-	
-	public String rechercheCompteByIdClient(){
+
+	public String rechercheCompteByIdClient() {
 		listCpt = compteService.getCompteByIdCLient(client.getId_client());
 		return "listeCompte.xhtml";
 	}
-	
+
 	public String addCpt() {
-		compteService.AjouterCompte(cpt);;
+		compteService.AjouterCompte(cpt);
+		;
 		return null;
 	}
 
@@ -306,11 +268,11 @@ public class GestionnaireManagedBean implements Serializable{
 	}
 
 	public String deleteCpt() {
-		
+
 		compteService.SupprimerCompte(cpt);
 		return null;
 	}
-	
+
 	public String addCarte() {
 		carteService.AjouterCarteService(carte);
 		return null;
@@ -322,13 +284,13 @@ public class GestionnaireManagedBean implements Serializable{
 	}
 
 	public String deleteCarte() {
-		
+
 		carteService.SupprimerCarteService(carte);
 		return null;
 	}
-	
+
 	public String addClient() {
-		
+
 		client.setRole("client");
 		client.setGestionnaire(gestionnaire);
 		clientService.addClientService(client);
@@ -342,47 +304,54 @@ public class GestionnaireManagedBean implements Serializable{
 		return "accueilGestionnaire.xhtml";
 	}
 
-	public String deleteClient() {		
+	public String deleteClient() {
 		clientService.deleteClientService(client.getId_client());
 		listClient = clientService.getClientsByIdGestionnaireService(gestionnaire.getId_gestionnaire());
 		return "accueilGestionnaire.xhtml";
 	}
-	public String IsExist(){
-		int verif  = gestionnaireService.isExistGestionnaireService(gestionnaire.getLogin(), gestionnaire.getPassword());
-		
-		if (verif==1){
-					
-			gestionnaire = gestionnaireService.getGestByIdentificationService(gestionnaire.getLogin(), gestionnaire.getPassword());
-			
-			
+
+	public String IsExist() {
+		int verif = gestionnaireService.isExistGestionnaireService(gestionnaire.getLogin(), gestionnaire.getPassword());
+
+		if (verif == 1) {
+
+			gestionnaire = gestionnaireService.getGestByIdentificationService(gestionnaire.getLogin(),
+					gestionnaire.getPassword());
+
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("gestionnaire", gestionnaire);
-			
+
 			System.out.println(gestionnaire.getId_gestionnaire());
-			
+
 			listClient = clientService.getClientsByIdGestionnaireService(gestionnaire.getId_gestionnaire());
-			
+
 			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("listClient", listClient);
-			
+
 			return "accueilGestionnaire.xhtml";
-			
-		}else{
-			
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Le login ou le mot de passe est incorrect !!"));
-			
+
+		} else {
+
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Le login ou le mot de passe est incorrect !!"));
+
 			return "connexionGestionnaire.xhtml";
 		}
 	}
-	
-	public String aiguillageAccueil(){
+
+	public String aiguillageAccueil() {
 		listClient = clientService.getClientsByIdGestionnaireService(gestionnaire.getId_gestionnaire());
 		return "accueilGestionnaire.xhtml";
 	}
-	
-	public String aiguillageFormModifClient(){
+
+	public String aiguillageFormModifClient() {
 		listeGestionnaires = gestionnaireService.getAllGestionnairesService();
 		return "formModifClient.xhtml";
 	}
-	//----------------------------------------------------------------------------------------------------------------
-	//----------------------------------------------------------------------------------------------------------------
-	
+
+	public String aiguillageAjoutClient() {
+		this.client = new Client();
+		return "ajoutClient.xhtml";
+	}
+	// ----------------------------------------------------------------------------------------------------------------
+	// ----------------------------------------------------------------------------------------------------------------
+
 }
