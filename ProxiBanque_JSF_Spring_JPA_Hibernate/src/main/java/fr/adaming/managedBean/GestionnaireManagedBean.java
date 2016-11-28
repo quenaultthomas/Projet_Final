7792e68d5e2cc6d42d24165e841f76e6784d12ec
inti0227 <inti0227@DESKTOP-S9V0O4L.home>
2016-11-28 10:51:54 +0100

    victor : commit gestcrud fini

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/managedBean/GestionnaireManagedBean.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/managedBean/GestionnaireManagedBean.java
@@ -381,6 +381,12 @@ public class GestionnaireManagedBean implements Serializable{
 		listeGestionnaires = gestionnaireService.getAllGestionnairesService();
 		return "formModifClient.xhtml";
 	}
+	public String aiguillageFormModifCmtp(){
+		listClient = clientService.getAllClientService();
+		return "modificationCompte.xhtml";
+		
+		
+	}
 	
 	public String aiguillageAjoutClient() {
 		this.client = new Client();
