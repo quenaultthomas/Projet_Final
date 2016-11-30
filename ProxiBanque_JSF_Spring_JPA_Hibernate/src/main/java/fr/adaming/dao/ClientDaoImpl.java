360554d1704140fbb23bd740c436f6679179620e
inti0293 <inti0293@DESKTOP-4C50ANM.home>
2016-11-30 09:32:17 +0100

    Fabien : i drink and i know things !!! true story!!!

--- ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/dao/ClientDaoImpl.java
+++ ProxiBanque_JSF_Spring_JPA_Hibernate/src/main/java/fr/adaming/dao/ClientDaoImpl.java
@@ -40,6 +40,11 @@ public class ClientDaoImpl implements IClientDao {
 	/**
 	 * 4_Méthodes
 	 */
+	/**
+	 * Méthode addClientDao permet d'ajouter un client dans la database
+	 * 
+	 * Aucun retour
+	 */
 	@Override
 	public void addClientDao(Client client) {		
 
@@ -48,13 +53,23 @@ public class ClientDaoImpl implements IClientDao {
 
 	}
 
+	/**
+	 * Méthode updateClientDao permet de mettre à jour un client dans la database
+	 * 
+	 * Aucun retour
+	 */
 	@Override
 	public void updateClientDao(Client client) {
 		//Instanciation d'un objet utilisation
 		em.merge(client);
 
 	}
-
+	
+	/**
+	 * Méthode updateClientDao permet de mettre à jour un client dans la database
+	 * 
+	 * Aucun retour
+	 */
 	@Override
 	public void deleteClientDao(int id_client) {
 		//Récupérer un utilisateur par sont ID
@@ -64,7 +79,12 @@ public class ClientDaoImpl implements IClientDao {
 		em.remove(client);
 		
 	}
-
+	
+	/**
+	 * Méthode getAllClientDao permet de récuperer tous les clients de la database
+	 * 
+	 * Retourne une liste de client("List<Client>")
+	 */
 	@Override
 	public List<Client> getAllClientDao() {
 
@@ -75,6 +95,11 @@ public class ClientDaoImpl implements IClientDao {
 		return liste;
 	}
 
+	/**
+	 * Méthode getClientByIdDao permet de récuperer un client de la database avec sont Id
+	 * 
+	 * Retourne un objet client
+	 */
 	@Override
 	public Client getClientByIdDao(int id_client) {
 		//Récupérer un utilisateur par sont ID
@@ -83,6 +108,11 @@ public class ClientDaoImpl implements IClientDao {
 		return client;
 	}
 	
+	/**
+	 * Méthode isExistClientDao permet de vérifier si les identifiants existent dans la database
+	 * 
+	 * Retourne int (si le client existe le retour est 1 sinon 0)
+	 */
 	@Override
 	public int isExistClientDao(String mail, String password) {
 		String req="SELECT * FROM client c WHERE c.mail=:mail and c.password=:password";
@@ -97,6 +127,11 @@ public class ClientDaoImpl implements IClientDao {
 		return listeClient.size();
 	}
 	
+	/**
+	 * Méthode getClientByIdGestionnaireDao permet de récupérer tout les clients d'un gestionnaire grace à son Id
+	 * 
+	 * Retourne une liste de client("List<Client>")
+	 */
 	public List<Client> getClientsByIdGestionnaireDao(int id_gest){
 		String req="SELECT c FROM Client c WHERE c.gestionnaire.id_gestionnaire=:id_gest";
 		
@@ -106,6 +141,11 @@ public class ClientDaoImpl implements IClientDao {
 		return query.getResultList();
 	}
 	
+	/**
+	 * Méthode getClientByIdentificationDao permet de récupérer un client grace à ses identifiants 
+	 * 
+	 * Retourne un objet client
+	 */
 	@Override
 	public Client getClientByIdentifiantDao(String mail, String password) {
 		String req="SELECT c FROM Client c WHERE c.mail=:mail and c.password=:password";
@@ -117,7 +157,12 @@ public class ClientDaoImpl implements IClientDao {
 		Client client = (Client) query.getSingleResult();
 		return client;
 	}
-
+	
+	/**
+	 * Méthode getClientByNomPrenom permet de récupérer un client grace à son nom et prenom 
+	 * 
+	 * Retourne un objet client
+	 */
 	@Override
 	public Client getClientByNomPrenom(String nom, String prenom) {
 	String req="SELECT c FROM Client c WHERE c.nom=:nom and c.prenom=:prenom";
